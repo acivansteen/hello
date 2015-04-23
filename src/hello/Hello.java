624cadbd3d4086f452d25f5dfaac97d3a529f02c
acivansteen <bram_vansteen@hotmail.com>
2015-04-23 14:37:53 +0200

    niew en verbeterd,nu met getalletje

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -4,6 +4,10 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("hello world");
+		int bram=0;
+		int count=bram+5;
+		System.out.println(count);
+		
 	}
 
 }
